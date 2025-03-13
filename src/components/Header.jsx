ff93e16e6ee6dbecafc60ea76d5d2a341421f8b7
Wissem Jderi <asqr43641@gmail.com>
2025-03-13 11:59:15 +0100

    update title and restructure app with header component for note taking functionality

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import "../styles/header.css";
+function Header() {
+  return (
+    <div className="header">
+      <nav>
+        <section>
+          <h1>عــرض الفــوائــــــد</h1>
+          <h1>عــرض أقســام الفــوائــــــد</h1>
+        </section>
+        <h1>كــنــاشــــة الفــوائــــــد</h1>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
